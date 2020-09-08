6963b563b2db1eb968fd34e73af2bac22b5c87b8
Urena <urenamar@amazon.com>
2020-09-07 22:52:17 -0700

    Transferring to personal laptop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import HomePage from './components/homepage.component';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <HomePage/>
+    </div>;
   }
 }
 
---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const HomePage = props => (
+    <div className="homepage">
+        <div className="directory-menu">
+        <div className="menu-item">
+                <div className="content">
+                    <h1>HATS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1>JACKETS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1>WOMENS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1>MENS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
