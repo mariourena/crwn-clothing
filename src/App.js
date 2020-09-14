e4e89b9667ca4df950f9cf52902839efd8719950
Mario Urena <=>
2020-09-13 18:39:10 -0700

    Create Route Link for nav:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = props => (
+  <div>Hats Page</div>
+)
 
 class App extends Component {
   render() {
     return <div className='App'>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>;
   }
 }
